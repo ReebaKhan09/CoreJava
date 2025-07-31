eb226562fbd5e4aca588d71483bdbb7dbb649a55
ReebaKhan09 <khan.reeba2020@email.com>
2025-07-31 21:44:35 +0530

    Collection

---
+++ src/com/collection/ArrayListTest.java
@@ -0,0 +1,12 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+ArrayList<Integer> arrayList = new ArrayList<>();
+arrayList.add(1);
+System.out.println(arrayList);
+	}
+}
